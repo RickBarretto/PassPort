2c24318dc9a14f06a9f4d2a85e47d91cfaa7e901
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-10 15:28:55 -0300

    deprecate DesktopApplication

---
+++ src/main/java/passport/application/desktop/App.java
@@ -0,0 +1,29 @@
+package passport.application.desktop;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import passport.application.desktop.welcome.WelcomeWindow;
+
+public class App extends Application {
+    @Override
+    public void start(Stage root) {
+        WelcomeWindow welcomeWindow = new WelcomeWindow();
+        Scene scene = new Scene(welcomeWindow, 1200, 700);
+
+        // @formatter:off
+        var css = scene.getStylesheets();
+        var mainCSS = getClass().getResource("/desktop/styles.css").toExternalForm();
+        var languageSelectorCSS = getClass().getResource("/desktop/language-selector.css").toExternalForm();
+        // @formatter:on
+
+        css.add(mainCSS);
+        css.add(languageSelectorCSS);
+
+        root.setTitle("PassPort");
+        root.setScene(scene);
+        root.show();
+    }
+
+    public static void main(String[] args) { launch(args); }
+}
