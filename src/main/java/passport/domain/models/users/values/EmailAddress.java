3e75c24804e47612ceddcc814d7817fc65748625
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-11 09:53:57 -0300

    minor fix

--- src/main/java/passport/domain/models/users/values/EmailAddress.java
+++ src/main/java/passport/domain/models/users/values/EmailAddress.java
@@ -1,4 +1,9 @@
 package passport.domain.models.users.values;
 
 public record EmailAddress(String value) {
+
+    public boolean equals(String value) {
+        return this.value.equals(value);
+    }
+
 }
