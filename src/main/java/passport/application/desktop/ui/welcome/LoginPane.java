0282edca20c9f2684b1f97962ab9135c019b80f4
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-11 07:52:32 -0300

    minor fix

--- src/main/java/passport/application/desktop/ui/welcome/LoginPane.java
+++ src/main/java/passport/application/desktop/ui/welcome/LoginPane.java
@@ -8,7 +8,7 @@ import passport.application.desktop.Translator;
 
 public class LoginPane extends VBox {
     private final Label title;
-    private final TextField username;
+    private final TextField email;
     private final PasswordField password;
     private final Button loginButton;
     private final Button switchToLogon;
@@ -19,7 +19,7 @@ public class LoginPane extends VBox {
         this.parent = parent;
 
         title = new Label();
-        username = new TextField();
+        email = new TextField();
         password = new PasswordField();
         loginButton = new Button();
         switchToLogon = new Button();
@@ -44,7 +44,7 @@ public class LoginPane extends VBox {
         setAlignment(Pos.CENTER);
         getStyleClass().add("form-container");
 
-        getChildren().addAll(title, username, password, loginButton,
+        getChildren().addAll(title, email, password, loginButton,
                 new Separator(), switchToLogon);
     }
 
@@ -52,7 +52,7 @@ public class LoginPane extends VBox {
         // @formatter:off
         Translator.instance()
             .translateFrom(title::setText, "login.title")
-            .translateFrom(username::setPromptText, "login.username")
+            .translateFrom(email::setPromptText, "login.email")
             .translateFrom(password::setPromptText, "login.password")
             .translateFrom(loginButton::setText, "login.button")
             .translateFrom(switchToLogon::setText, "login.switch");
