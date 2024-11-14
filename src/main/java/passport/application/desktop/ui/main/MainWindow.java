c2d73060f466a9ff47aefa2759ce9968a62247f1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-14 18:20:10 -0300

    fix compilation

--- src/main/java/passport/application/desktop/ui/main/MainWindow.java
+++ src/main/java/passport/application/desktop/ui/main/MainWindow.java
@@ -2,13 +2,10 @@ package passport.application.desktop.ui.main;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import passport.application.desktop.Infra;
 
 public class MainWindow extends VBox {
-    final Infra infra;
     
-    public MainWindow(Infra infra) {
-        this.infra = infra;
+    public MainWindow() {
         setupUI();
     }
     
