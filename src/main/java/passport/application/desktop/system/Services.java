afe72ee7b8e38bcf0c0d19e49000f10351caa69b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-16 16:41:40 -0300

    add evaluation

--- src/main/java/passport/application/desktop/system/Services.java
+++ src/main/java/passport/application/desktop/system/Services.java
@@ -1,10 +1,12 @@
 package passport.application.desktop.system;
 
 import passport.domain.contexts.events.AvailableEventsListing;
+import passport.domain.contexts.events.EventEvaluation;
 import passport.domain.contexts.user.SigningUp;
 import passport.domain.contexts.user.UserLogin;
 
 public record Services(
         SigningUp signup,
         UserLogin login,
-        AvailableEventsListing eventsListing ) {}
+        AvailableEventsListing eventsListing,
+        EventEvaluation evaluation) {}
