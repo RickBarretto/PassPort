c4f358f9357d019218f7811b74d10386a9131fcb
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-17 20:00:22 -0300

    implement service

---
+++ src/main/java/passport/domain/contexts/events/EvaluationListing.java
@@ -0,0 +1,47 @@
+package passport.domain.contexts.events;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import passport.domain.models.events.Event;
+import passport.roles.Context;
+import passport.roles.repositories.Users;
+
+/**
+ * Context for evaluating an event.
+ */
+public class EvaluationListing implements Context {
+    private Users users;
+
+    public record Comment(String name, String content) {
+        @Override
+        public String toString() { return name + ": " + content; }
+    }
+
+    /**
+     * Constructor with the specified events repository.
+     *
+     * @param events the repository of events used for evaluation
+     */
+    public EvaluationListing(Users users) { this.users = users; }
+
+    /**
+     * Sets the event ID for the evaluation.
+     *
+     * @param event the event
+     * @return the updated EvaluationListing object
+     */
+    public List<Comment> of(Event event) {
+        var evaluations = event.evaluations();
+        return evaluations.stream()
+                .map(eval -> {
+                    final String username = users.byId(eval.author()).get()
+                            .person()
+                            .name();
+
+                    return new Comment(username, eval.comment());
+                })
+                .collect(Collectors.toList());
+    }
+
+}
