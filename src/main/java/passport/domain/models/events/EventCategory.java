275d1d9e8ba6cc4afb07936181a1213f91b119f5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-17 21:15:15 -0300

    add toString for event category enum

--- src/main/java/passport/domain/models/events/EventCategory.java
+++ src/main/java/passport/domain/models/events/EventCategory.java
@@ -1,12 +1,8 @@
 package passport.domain.models.events;
 
 public enum EventCategory {
-    CINE,
-    CONFERENCE,
-    FESTIVAL,
-    MUSIC,
-    NOT_PROVIDED,
-    SPORTS,
-    THEATRE,
-    WORKSHOP
+    CINE, CONFERENCE, FESTIVAL, MUSIC, NOT_PROVIDED, SPORTS, THEATRE, WORKSHOP;
+
+    @Override
+    public String toString() { return "category." + this.name().toLowerCase(); }
 }
