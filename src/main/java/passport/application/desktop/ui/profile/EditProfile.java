db3f8a98ed792755ba83e1141beb27bb879d984b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-24 22:13:57 -0300

    minor fix

--- src/main/java/passport/application/desktop/ui/profile/EditProfile.java
+++ src/main/java/passport/application/desktop/ui/profile/EditProfile.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
