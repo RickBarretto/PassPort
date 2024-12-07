1dd24d4fbadd24b886e66e20c827c22ef54a6c87
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-07 18:41:39 -0300

    add javadoc

--- src/main/java/passport/application/desktop/ui/welcome/Hero.java
+++ src/main/java/passport/application/desktop/ui/welcome/Hero.java
@@ -4,24 +4,47 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 
+/**
+ * Pane "Hero" da janela de boas-vindas.
+ */
 public class Hero extends StackPane {
+
+    /**
+     * Inicializa a interface do componente.
+     */
     public Hero() { setupUI(); }
 
     // =~=~=~=~= =~=~=~=~= SETUP UI =~=~=~=~= =~=~=~=~=
 
+    /**
+     * Configura a interface do componente
+     */
     void setupUI() {
         setupCss();
         var appName = appName();
         addChildren(appName);
     }
 
+    /**
+     * Adiciona os elementos filhos.
+     *
+     * @param appName O rótulo com o nome do aplicativo a ser adicionado.
+     */
     private void addChildren(Label appName) { getChildren().add(appName); }
 
+    /**
+     * Configura o estilo CSS do componente.
+     */
     private void setupCss() {
         setAlignment(Pos.CENTER);
         getStyleClass().add("hero");
     }
 
+    /**
+     * Retorna um Label com o nome do aplicativo.
+     *
+     * @return Um Label configurado com o nome do aplicativo.
+     */
     private Label appName() {
         Label appName = new Label("PassPort");
         appName.getStyleClass().add("app-name");
--- src/main/java/passport/application/desktop/ui/welcome/WelcomeWindow.java
+++ src/main/java/passport/application/desktop/ui/welcome/WelcomeWindow.java
@@ -10,12 +10,20 @@ import passport.application.desktop.ui.components.LanguageSelector;
 import passport.application.desktop.ui.welcome.login.LoginForm;
 import passport.application.desktop.ui.welcome.signup.SignupForm;
 
+/**
+ * Classe responsável por configurar a janela de boas-vindas da aplicação.
+ */
 public class WelcomeWindow extends HBox {
     private final Hero hero;
     private final LoginForm login;
     private final SignupForm signUpForm;
     private final LanguageSelector languageSelector;
 
+    /**
+     * Inicializa a janela de boas-vindas.
+     *
+     * @param app A instância da aplicação PassPort.
+     */
     public WelcomeWindow(PassPort app) {
         hero = new Hero();
         login = new LoginForm(app, this::switchToLogon);
@@ -27,22 +35,33 @@ public class WelcomeWindow extends HBox {
 
     // =~=~=~=~= =~=~=~=~= SETUP ACTIONS =~=~=~=~= =~=~=~=~=
 
+    /**
+     * Substitui o conteúdo do painel direito pelo nó fornecido. Isso permite a
+     * troca entre os painéis de Login e SignUp.
+     *
+     * @param node O nó a ser exibido no painel direito.
+     */
     private void rightPaneWith(Node node) {
         var rightPane = (VBox) getChildren().get(1);
         var mainContent = (VBox) rightPane.getChildren().get(1);
         mainContent.getChildren().set(0, node);
     }
 
-    public void switchToLogon() {
-        this.rightPaneWith(this.signUpForm);
-    }
+    /**
+     * Alterna para o formulário de Signup.
+     */
+    public void switchToLogon() { this.rightPaneWith(this.signUpForm); }
 
-    public void switchToLogin() {
-        this.rightPaneWith(this.login);
-    }
+    /**
+     * Alterna para o formulário de Login.
+     */
+    public void switchToLogin() { this.rightPaneWith(this.login); }
 
     // =~=~=~=~= =~=~=~=~= SETUP UI =~=~=~=~= =~=~=~=~=
 
+    /**
+     * Configura a interface do usuário da janela de boas-vindas.
+     */
     private void setupUI() {
         var mainContent = mainContent();
         var rightPane = rightPane(mainContent);
@@ -52,19 +71,30 @@ public class WelcomeWindow extends HBox {
         getChildren().addAll(hero, rightPane);
     }
 
+    /**
+     * Cria o painel direito com o seletor de idioma e o conteúdo principal.
+     *
+     * @param mainContent O conteúdo principal a ser exibido.
+     * @return Um VBox configurado como painel direito.
+     */
     private VBox rightPane(VBox mainContent) {
         VBox rightPane = new VBox();
         rightPane.getChildren().addAll(languageSelector, mainContent);
         return rightPane;
     }
 
+    /**
+     * Cria o conteúdo principal da janela de boas-vindas.
+     *
+     * @return Um VBox configurado como conteúdo principal.
+     */
     private VBox mainContent() {
         VBox mainContent = new VBox();
 
         VBox.setVgrow(mainContent, Priority.ALWAYS);
         mainContent.setAlignment(Pos.CENTER);
         mainContent.getChildren().add(login);
-        
+
         return mainContent;
     }
 }
